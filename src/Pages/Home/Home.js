823b0040d0215fe6baf4765b4052006787307513
Akash <na.akash715@gmail.com>
2022-10-25 21:38:53 +0600

    home done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import carousel1 from "../../assets/carouselImages/carousel1.jpg";
 import carousel2 from "../../assets/carouselImages/carousel2.jpg";
 import carousel3 from "../../assets/carouselImages/carousel3.jpg";
 import carousel4 from "../../assets/carouselImages/carousel4.jpg";
+import './Home.css';
+import { FaDatabase, FaGlobe, FaDesktop, FaMobileAlt, FaReact, FaRobot, FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
     return (
@@ -47,6 +50,98 @@ const Home = () => {
                     </Carousel.Item>
                 </Carousel>
             </div>
+
+            <Container className='get-started-container mt-5 pb-5 d-flex flex-column flex-lg-row flex-md-column'>
+                <div className='get-started text-start my-5'>
+                    <p className='display-6 text-start p-4'>Get Started with Code Monkey Today to get a
+                        <span className='text-primary fw-normal'> flat 50%</span> discount today.</p>
+                    <Button variant='danger' className='ms-4 px-5 py-3 fs-3'>Get Started</Button>
+                </div>
+                <div className='text-center me-5'>
+                    <div className='advertise-panel d-flex align-items-center my-5'>
+                        <p className='display-3 text-end mt-4 fw-normal'>
+                            Learn Coding Online With Professional Instructors
+                        </p>
+                    </div>
+                </div>
+
+            </Container>
+            <div>
+                <h4 className='display-3 py-3 my-5'>Join us to explore</h4>
+            </div>
+            <Container className='learn-with-us-container d-flex flex-column flex-sm-column flex-md-column flex-lg-row mb-5 mt-5'>
+                <div className='text-start '>
+                    <p className='fs-4 fw-normal text-start p-4 me-4 mt-3'>A programming class can teach you enough to mock up a very basic website design
+                        or an app you have an idea for.
+                        Or, in certain industries, coding chops can impress the hiring manager even if it’s not a part of your job.
+                        A little bit of knowledge lets them know that you understand current trends, that you’re proactive and curious,
+                        that you’ll be able to communicate with colleagues and clients about the technical aspects, and that just
+                        maybe you might be able to pitch in on bigger projects.</p>
+                    <Button variant='danger' className='ms-4 px-5 py-2 fs-3 pb-3'>Courses  <FaArrowRight></FaArrowRight></Button>
+                </div>
+
+                <Container className='options-container mt-5' >
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaDatabase></FaDatabase>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>DBMS Programming</h5>
+                            <p>Build Your Own DBMS with real time help sessions and our expart tutors.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaGlobe></FaGlobe>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>Data Science</h5>
+                            <p>Be the leader of data science and analytics. Get hired instantly.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaReact></FaReact>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>Web Design</h5>
+                            <p>Design real life industry applications with the leading libraries and get hired. </p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaDesktop></FaDesktop>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>Desktop App</h5>
+                            <p>Learn aboy desktop applications and make your own with Code Monkey.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaMobileAlt></FaMobileAlt>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>Mobile App</h5>
+                            <p>Code Monkey helps you to master android and ios platforms and using them.</p>
+                        </div>
+                    </div>
+
+                    <div className='d-flex flex-row border border-3 ps-2 pe-5 pt-3 options-div'>
+                        <div className='fs-3 options-card pt-1'>
+                            <FaRobot></FaRobot>
+                        </div>
+                        <div className='text-start d-flex flex-column justify-content-center'>
+                            <h5>CNN</h5>
+                            <p>Deep learning and Concolutional Neural Networks is the future. Join us to get Started.</p>
+                        </div>
+                    </div>
+                </Container>
+            </Container>
 
 
         </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './Footer.css';
+import logo22 from '../../../assets/images/logo22.png';
+import { Link } from 'react-router-dom';
+import { FaFacebookSquare, FaGithubSquare, FaGoogle, FaInstagram, FaLinkedin, FaWhatsappSquare } from "react-icons/fa";
+
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+            <section className="bg-dark" id="footer-contact">
+                <div className="row py-5">
+                    <div className="col-12 d-flex flex-column justify-content-center align-items-center ">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <img className="img-fluid w-25 pb-4" src={logo22} alt="" />
+                        </div>
+                        <p className="text-center text-info fs-6">Office 41, 5/5 Monipuri Para, <br /> Bijoy Sharoni, Dhaka</p>
+                        <div className="">
+                            <nav className="text-white d-flex footer-nav">
+                                <Link to='/terms' className='p-3'>Terms of Service</Link>
+                                <Link to='/policy' className='p-3'>Privacy Policy</Link>
+                            </nav>
+                            <nav>
+
+                            </nav>
+                        </div>
+                        <div className="platform-icons mb-3">
+                            <Link to='/google' className='fs-3 p-3'><FaGoogle></FaGoogle></Link>
+                            <Link to='/git' className='fs-3 p-3'><FaGithubSquare></FaGithubSquare></Link>
+                            <Link to='linkedin' className='fs-3 p-3'><FaLinkedin></FaLinkedin> </Link>
+                            <Link to='/insta' className='fs-3 p-3'><FaInstagram></FaInstagram> </Link>
+                            <Link to='/facebook' className='fs-3 p-3'><FaFacebookSquare></FaFacebookSquare> </Link>
+                            <Link to='/wapp' className='fs-3 p-3'><FaWhatsappSquare></FaWhatsappSquare> </Link>
+
+                        </div>
+                        <span className=" mt-2 fw-normal text-info fs-6" id="copy-right">codeMonkeyⒸNasin A Akash-2022</span>
+
+
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
