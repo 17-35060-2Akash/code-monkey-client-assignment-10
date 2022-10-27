541f8644029097aeb881b52b6eb83efb38a57c07
Akash <na.akash715@gmail.com>
2022-10-27 20:28:08 +0600

    last commit

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { FaFilePdf } from "react-icons/fa";
 import { FaStar, FaClock, FaArrowRight } from "react-icons/fa";
 import './CourseDetails.css';
 import { Button, Container } from 'react-bootstrap';
-import jsPDF from 'jspdf';
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+
+/* import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-import PDFContent from '../Others/PDFContent';
+import PDFContent from '../Others/PDFContent'; */
 
 
 const CourseDetails = () => {
@@ -15,8 +17,22 @@ const CourseDetails = () => {
     const { id, name, image, details, ratings, duration, curriculam } = course;
     // const [open, setOpen] = useState(false);
 
+    const pdfExportComponentLarge = useRef(null);
 
-    const generatePDF = () => {
+    const handleExportPdfLarge = (event) => {
+        pdfExportComponentLarge.current.save();
+    };
+
+    const pdfExportComponentSmall = useRef(null);
+
+    const handleExportPdfSamll = (event) => {
+        pdfExportComponentSmall.current.save();
+    };
+
+
+
+
+    /* const generatePDF = () => {
         const doc = new jsPDF('p', 'pt', 'a4', true);
         doc.html(document.querySelector('#card-to-pdf'), {
             callback: function (pdf) {
@@ -27,107 +43,103 @@ const CourseDetails = () => {
         });
 
 
-    };
+    }; */
 
-    /*   const generatePDF = () => {
-          const input = document.getElementById('card-to-pdf');
-          html2canvas(input, { logging: true, letterRendering: 1, useCORS: true }).then(canvas => {
-              const imgWidth = 208;
-              const imgHeight = canvas.height * imgWidth / canvas.width;
-              const imgData = canvas.toDataURL('img/png');
-              const pdf = new jsPDF('p', 'mm', 'a4');
-              pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-              pdf.save('mypdf.pdf');
-          })
-  
-      }; */
 
     return (
         <div className=''>
-            <Card className="bg-dark text-white details-card d-none d-sm-block d-md-block d-lg-block" >
-                <Card.Img src={image} alt="Card image" />
-                <Card.ImgOverlay className='card-writings' >
-                    <div>
-                        <div className='fs-3 pt-2 mt-2 me-2 d-flex align-items-center justify-content-end' title='Download PDF'>
-                            <FaFilePdf onClick={generatePDF} className='pdf-btn display-6'></FaFilePdf>
-                        </div>
+            <PDFExport ref={pdfExportComponentLarge} paperSize='a4'>
 
-                        <p className='course-title'>{name}</p>
-
-                        <div className='d-flex align-items-center justify-content-between  course-ratings'>
-                            <Card.Title className='d-flex align-items-center '>
-                                <FaStar className='text-warning rating-duration-text'></FaStar>
-                                <span className='ps-1 rating-duration-text'>{ratings}</span>
-                            </Card.Title>
-                            <Card.Title className='d-flex align-items-center rating-duration-text'>
-                                <FaClock className='text-info me-2 rating-duration-text'></FaClock>
-                                <span className='ps-1 rating-duration-text'>{duration}</span>
-                            </Card.Title>
-                        </div>
-                        <Card.Text className='card-details'>
-                            {details}
-                        </Card.Text>
-
-                        <div className='d-flex justify-content-around align-items-center flex-row flex-md-column flex-lg-column curriculam-details '>
-                            <div className='text-start'>
-                                {
-                                    curriculam.map((cur, idx) => <li className='py-1'
-                                        key={idx}>{cur}</li>)
-                                }
+                <Card className="bg-dark text-white details-card d-none d-sm-block d-md-block d-lg-block" >
+                    <Card.Img src={image} alt="Card image" />
+                    <Card.ImgOverlay className='card-writings' >
+                        <div>
+                            <div className='fs-3 pt-2 mt-2 me-2 d-flex align-items-center justify-content-end' title='Download PDF'>
+                                <FaFilePdf onClick={handleExportPdfLarge} className='pdf-btn display-6'></FaFilePdf>
                             </div>
 
 
-                            <div className=' flex-grow-1 align-items-center btn-container'>
-                                <Link to={`/checkout/${id}`}>
-                                    <Button className='my-bg-pink premium-btn px-5 py-3  d-flex align-items-center justify-content-center'><span className='rating-duration-text me-2'>Premium Access</span> <span><FaArrowRight className='rating-duration-text'></FaArrowRight></span></Button>
 
-                                </Link>
+                            <p className='course-title'>{name}</p>
+                            <div className='d-flex align-items-center justify-content-between  course-ratings'>
+                                <Card.Title className='d-flex align-items-center '>
+                                    <FaStar className='text-warning rating-duration-text'></FaStar>
+                                    <span className='ps-1 rating-duration-text'>{ratings}</span>
+                                </Card.Title>
+                                <Card.Title className='d-flex align-items-center rating-duration-text'>
+                                    <FaClock className='text-info me-2 rating-duration-text'></FaClock>
+                                    <span className='ps-1 rating-duration-text'>{duration}</span>
+                                </Card.Title>
                             </div>
+                            <Card.Text className='card-details'>
+                                {details}
+                            </Card.Text>
+
+
+                            <div className='d-flex justify-content-around align-items-center flex-row flex-md-column flex-lg-column curriculam-details '>
+                                <div className='text-start'>
+                                    {
+                                        curriculam.map((cur, idx) => <li className='py-1'
+                                            key={idx}>{cur}</li>)
+                                    }
+                                </div>
+
+
+                                <div className=' flex-grow-1 align-items-center btn-container'>
+                                    <Link to={`/checkout/${id}`}>
+                                        <Button className='my-bg-pink premium-btn px-5 py-3  d-flex align-items-center justify-content-center'><span className='rating-duration-text me-2'>Premium Access</span> <span><FaArrowRight className='rating-duration-text'></FaArrowRight></span></Button>
+
+                                    </Link>
+                                </div>
+                            </div>
+
+
+                            <Card.Footer>
+                            </Card.Footer>
                         </div>
-
-
-                        <Card.Footer>
-                        </Card.Footer>
-                    </div>
-                </Card.ImgOverlay>
-            </Card>
-
+                    </Card.ImgOverlay>
+                </Card>
+            </PDFExport>
 
 
             <Container className='small-screen-card d-block d-sm-none d-md-none d-lg-none d-flex justify-content-around align-items-center my-5 py-5'>
                 <Card className='border-0 ' style={{ width: '22rem' }} >
-                    <Card.Img variant="top" src={image} className='' />
-                    <Card.Body id='card-to-pdf'>
+                    <PDFExport ref={pdfExportComponentSmall} paperSize='a4'>
+                        <Card.Img variant="top" src={image} className='' />
+                        <Card.Body id='card-to-pdf'>
 
 
-                        <div >
-                            <div className='fs-3 pt-2  d-flex align-items-center justify-content-end' title='Download PDF'>
-                                <FaFilePdf onClick={generatePDF} className='pdf-btn display-6 text-danger'></FaFilePdf>
-                            </div>
-                            <Card.Title className='display-6 mt-3 fw-normal'>{name}</Card.Title>
-
-
-                            <Card.Text className='mt-2'>
-                                {details}
-                            </Card.Text>
-                            <div>
-                                <div className='text-start py-2 pt-3 ps-1'>
-                                    {
-                                        curriculam.map((cur, idx) => <li className=''
-                                            key={idx}>{cur}</li>)
-                                    }
+                            <div >
+                                <div className='fs-3 pt-2  d-flex align-items-center justify-content-end' title='Download PDF'>
+                                    <FaFilePdf onClick={handleExportPdfSamll} className='pdf-btn display-6 text-danger'></FaFilePdf>
                                 </div>
+                                <Card.Title className='display-6 mt-3 fw-normal'>{name}</Card.Title>
+
+
+                                <Card.Text className='mt-2'>
+                                    {details}
+                                </Card.Text>
+                                <div>
+                                    <div className='text-start py-2 pt-3 ps-1'>
+                                        {
+                                            curriculam.map((cur, idx) => <li className=''
+                                                key={idx}>{cur}</li>)
+                                        }
+                                    </div>
+                                </div>
+                                {/*  */}
+
                             </div>
-                            <div className=' flex-grow-1 align-items-center '>
 
-                                <Link to={`/checkout/${id}`}>
-                                    <Button className='my-bg-pink px-4 py-2 fs-6 my-4'>Premium Access <FaArrowRight></FaArrowRight></Button>
+                        </Card.Body>
+                    </PDFExport>
+                    <div className=' flex-grow-1 align-items-center '>
 
-                                </Link>
-                            </div>
-                        </div>
+                        <Link to={`/checkout/${id}`}>
+                            <Button className='my-bg-pink px-4 py-2 fs-6 my-4'>Premium Access <FaArrowRight></FaArrowRight></Button>
 
-                    </Card.Body>
+                        </Link>
+                    </div>
                 </Card>
             </Container>
             {/* <div className='' >
